7bab3d7cb0b9dfc4437aa24cb98fdd0b218cae1d
HeXis-YS <40174982+HeXis-YS@users.noreply.github.com>
2026-04-08 01:02:12 +0800

    refactor: remove empty legacy placeholders

--- app/src/main/java/ceui/lisa/database/IllustTask.kt
+++
@@ -1,9 +0,0 @@
-package ceui.lisa.database
-
-import ceui.lisa.core.DownloadItem
-import ceui.lisa.models.IllustsBean
-
-class IllustTask {
-    var downloadTask: DownloadItem? = null
-    var illustsBean: IllustsBean? = null
-}
