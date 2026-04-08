b7a71538f60cb09075fc8e9b5e2303af1c2271ee
HeXis-YS <40174982+HeXis-YS@users.noreply.github.com>
2026-04-09 01:51:15 +0800

    refactor: migrate filename and novel detail fragments to kotlin

---
+++ app/src/main/java/ceui/lisa/fragments/FragmentFileName.kt
@@ -0,0 +1,160 @@
+package ceui.lisa.fragments
+
+import android.text.TextUtils
+import android.view.View
+import android.widget.CompoundButton
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import ceui.lisa.R
+import ceui.lisa.activities.Shaft
+import ceui.lisa.adapters.FileNameAdapter
+import ceui.lisa.databinding.FragmentFileNameBinding
+import ceui.lisa.download.FileCreator
+import ceui.lisa.interfaces.OnItemClickListener
+import ceui.lisa.model.CustomFileNameCell
+import ceui.lisa.models.IllustsBean
+import ceui.lisa.utils.Common
+import ceui.lisa.utils.DensityUtil
+import ceui.lisa.utils.Local
+import ceui.lisa.utils.Params
+import ceui.lisa.view.LinearItemDecoration
+import com.google.gson.reflect.TypeToken
+import com.scwang.smart.refresh.layout.SmartRefreshLayout
+import java.util.ArrayList
+import java.util.Collections
+
+class FragmentFileName : SwipeFragment<FragmentFileNameBinding>() {
+    private lateinit var illust: IllustsBean
+    private val allItems = ArrayList<CustomFileNameCell>()
+    private var mAdapter: FileNameAdapter? = null
+
+    companion object {
+        @JvmStatic
+        fun newInstance(): FragmentFileName = FragmentFileName()
+    }
+
+    override fun initLayout() {
+        mLayoutID = R.layout.fragment_file_name
+    }
+
+    override fun initView() {
+        illust = Shaft.sGson.fromJson(Params.EXAMPLE_ILLUST, IllustsBean::class.java)
+        baseBind.toolbar.toolbar.setNavigationOnClickListener { mActivity.finish() }
+        baseBind.toolbar.toolbar.setTitle(R.string.string_242)
+        baseBind.showNow.setOnClickListener { showPreview() }
+        baseBind.saveNow.setOnClickListener { saveSettings() }
+        baseBind.reset.setOnClickListener {
+            if (mAdapter != null) {
+                allItems.clear()
+                allItems.addAll(FileCreator.defaultFileCells())
+                mAdapter!!.notifyDataSetChanged()
+            }
+        }
+        baseBind.hasP0.isChecked = Shaft.sSettings.isHasP0
+        baseBind.hasP0.setOnCheckedChangeListener { _: CompoundButton, isChecked: Boolean ->
+            Shaft.sSettings.isHasP0 = isChecked
+            Common.showToast(getString(R.string.string_428))
+            Local.setSettings(Shaft.sSettings)
+        }
+    }
+
+    override fun initData() {
+        allItems.clear()
+        val defaultFileCells = FileCreator.defaultFileCells()
+        if (TextUtils.isEmpty(Shaft.sSettings.fileNameJson)) {
+            allItems.addAll(defaultFileCells)
+        } else {
+            val customFileNameCells: Collection<CustomFileNameCell> = Shaft.sGson.fromJson(
+                Shaft.sSettings.fileNameJson,
+                object : TypeToken<List<CustomFileNameCell>>() {}.type,
+            )
+            allItems.addAll(customFileNameCells)
+            val set = allItems.map { it.code }.toSet()
+            allItems.addAll(defaultFileCells.filter { !set.contains(it.code) })
+        }
+
+        mAdapter = FileNameAdapter(allItems, mContext)
+        baseBind.recyclerView.layoutManager = LinearLayoutManager(mContext)
+        baseBind.recyclerView.isNestedScrollingEnabled = true
+        baseBind.recyclerView.addItemDecoration(LinearItemDecoration(DensityUtil.dp2px(12.0f)))
+        baseBind.recyclerView.adapter = mAdapter
+        object : ItemTouchHelper.Callback() {
+            override fun getMovementFlags(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+            ): Int {
+                val dragFlags = ItemTouchHelper.UP or ItemTouchHelper.DOWN
+                return makeMovementFlags(dragFlags, 0)
+            }
+
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder,
+            ): Boolean {
+                Collections.swap(allItems, viewHolder.adapterPosition, target.adapterPosition)
+                return true
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+            }
+
+            override fun onMoved(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                fromPos: Int,
+                target: RecyclerView.ViewHolder,
+                toPos: Int,
+                x: Int,
+                y: Int,
+            ) {
+                super.onMoved(recyclerView, viewHolder, fromPos, target, toPos, x, y)
+                mAdapter!!.notifyItemMoved(viewHolder.adapterPosition, target.adapterPosition)
+                showPreview()
+            }
+        }.let { ItemTouchHelper(it).attachToRecyclerView(baseBind.recyclerView) }
+        mAdapter!!.setOnItemClickListener(object : OnItemClickListener {
+            override fun onItemClick(v: View?, position: Int, viewType: Int) {
+                showPreview()
+            }
+        })
+
+        showPreview()
+    }
+
+    private fun showPreview() {
+        for (allItem in allItems) {
+            if (allItem.code == FileCreator.ILLUST_ID && !allItem.isChecked) {
+                Common.showToast("作品ID为必选项，请选择作品ID")
+                return
+            }
+            if (allItem.code == FileCreator.P_SIZE && !allItem.isChecked) {
+                Common.showToast("作品P数为必选项，请选择作品P数")
+                return
+            }
+        }
+        val name = FileCreator.customFileNameForPreview(illust, allItems, 1)
+        baseBind.fileName.text = name
+    }
+
+    private fun saveSettings() {
+        for (allItem in allItems) {
+            if (allItem.code == FileCreator.ILLUST_ID && !allItem.isChecked) {
+                Common.showToast("作品ID为必选项，请选择作品ID")
+                return
+            }
+            if (allItem.code == FileCreator.P_SIZE && !allItem.isChecked) {
+                Common.showToast("作品P数为必选项，请选择作品P数")
+                return
+            }
+        }
+
+        val json = Shaft.sGson.toJson(allItems)
+        Shaft.sSettings.fileNameJson = json
+        Local.setSettings(Shaft.sSettings)
+        Common.showToast("保存成功！")
+    }
+
+    override fun getSmartRefreshLayout(): SmartRefreshLayout = baseBind.refreshLayout
+}
---
+++ app/src/main/java/ceui/lisa/fragments/FragmentNovelSeriesDetail.kt
@@ -0,0 +1,234 @@
+package ceui.lisa.fragments
+
+import android.net.Uri
+import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.widget.Toolbar
+import androidx.databinding.ViewDataBinding
+import com.bumptech.glide.Glide
+import ceui.lisa.R
+import ceui.lisa.activities.BaseActivity
+import ceui.lisa.activities.Shaft
+import ceui.lisa.adapters.BaseAdapter
+import ceui.lisa.adapters.NAdapter
+import ceui.lisa.cache.Cache
+import ceui.lisa.core.BaseRepo
+import ceui.lisa.databinding.FragmentNovelSeriesBinding
+import ceui.lisa.download.IllustDownload
+import ceui.lisa.http.Retro
+import ceui.lisa.interfaces.Callback
+import ceui.lisa.model.ListNovelOfSeries
+import ceui.lisa.models.NovelBean
+import ceui.lisa.models.NovelDetail
+import ceui.lisa.models.NovelSeriesItem
+import ceui.lisa.models.UserBean
+import ceui.lisa.models.WebNovel
+import ceui.lisa.repo.NovelSeriesDetailRepo
+import ceui.lisa.utils.Common
+import ceui.lisa.utils.GlideUtil
+import ceui.lisa.utils.Params
+import ceui.lisa.utils.PixivOperate
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Response
+import java.util.TreeMap
+
+class FragmentNovelSeriesDetail :
+    NetListFragment<FragmentNovelSeriesBinding, ListNovelOfSeries, NovelBean>() {
+
+    private var seriesID = 0
+
+    companion object {
+        @JvmStatic
+        fun newInstance(seriesID: Int): FragmentNovelSeriesDetail {
+            val args = Bundle()
+            args.putInt(Params.ID, seriesID)
+            val fragment = FragmentNovelSeriesDetail()
+            fragment.arguments = args
+            return fragment
+        }
+    }
+
+    override fun initLayout() {
+        mLayoutID = R.layout.fragment_novel_series
+    }
+
+    override fun initToolbar(toolbar: Toolbar) {
+        super.initToolbar(toolbar)
+        toolbar.inflateMenu(R.menu.novel_series_download)
+        toolbar.setOnMenuItemClickListener(object : Toolbar.OnMenuItemClickListener {
+            override fun onMenuItemClick(item: MenuItem): Boolean {
+                if (item.itemId == R.id.batch_download) {
+                    for (novelBean in allItems) {
+                        if (novelBean.isLocalSaved()) {
+                            saveNovelToDownload(
+                                novelBean,
+                                Cache.get().getModel(
+                                    Params.NOVEL_KEY + novelBean.id,
+                                    NovelDetail::class.java,
+                                )!!,
+                            )
+                        } else {
+                            Retro.getAppApi()
+                                .getNovelDetailV2(Shaft.sUserModel.access_token, novelBean.id.toLong())
+                                .enqueue(object : retrofit2.Callback<ResponseBody> {
+                                    override fun onResponse(
+                                        call: Call<ResponseBody>,
+                                        response: Response<ResponseBody>,
+                                    ) {
+                                        object : WebNovelParser(response) {
+                                            override fun onNovelPrepared(
+                                                novelDetail: NovelDetail,
+                                                webNovel: WebNovel,
+                                            ) {
+                                                saveNovelToDownload(novelBean, novelDetail)
+                                            }
+                                        }
+                                    }
+
+                                    override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                                    }
+                                })
+                        }
+                    }
+                } else if (item.itemId == R.id.batch_download_as_one) {
+                    val taskContainer = TreeMap<Long, String>()
+                    val lineSeparator = System.lineSeparator()
+                    for (novelBean in allItems) {
+                        if (novelBean.isLocalSaved()) {
+                            val sb = lineSeparator + novelBean.title + " - " + novelBean.id + lineSeparator +
+                                Cache.get().getModel(
+                                    Params.NOVEL_KEY + novelBean.id,
+                                    NovelDetail::class.java,
+                                )!!.novel_text
+                            taskContainer[novelBean.id.toLong()] = sb
+                            if (taskContainer.size == allItems.size) {
+                                val content = taskContainer.values.joinToString(lineSeparator)
+                                saveNovelSeriesToDownload(mResponse.novel_series_detail!!, content)
+                            }
+                        } else {
+                            Retro.getAppApi()
+                                .getNovelDetailV2(Shaft.sUserModel.access_token, novelBean.id.toLong())
+                                .enqueue(object : retrofit2.Callback<ResponseBody> {
+                                    override fun onResponse(
+                                        call: Call<ResponseBody>,
+                                        response: Response<ResponseBody>,
+                                    ) {
+                                        object : WebNovelParser(response) {
+                                            override fun onNovelPrepared(
+                                                novelDetail: NovelDetail,
+                                                webNovel: WebNovel,
+                                            ) {
+                                                val sb = lineSeparator + novelBean.title + " - " + novelBean.id + lineSeparator +
+                                                    novelDetail.novel_text
+                                                taskContainer[novelBean.id.toLong()] = sb
+                                                if (taskContainer.size == allItems.size) {
+                                                    val content = taskContainer.values.joinToString(lineSeparator)
+                                                    saveNovelSeriesToDownload(mResponse.novel_series_detail!!, content)
+                                                }
+                                            }
+                                        }
+                                    }
+
+                                    override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                                    }
+                                })
+                        }
+                    }
+                }
+                return true
+            }
+        })
+    }
+
+    override fun initBundle(bundle: Bundle) {
+        seriesID = bundle.getInt(Params.ID)
+    }
+
+    override fun adapter(): BaseAdapter<*, out ViewDataBinding> = NAdapter(allItems, mContext, true)
+
+    override fun repository(): BaseRepo = NovelSeriesDetailRepo(seriesID)
+
+    override fun getToolbarTitle(): String = "小说系列"
+
+    override fun onResponse(listNovelOfSeries: ListNovelOfSeries) {
+        try {
+            baseBind.cardPixiv.visibility = View.VISIBLE
+            baseBind.seriesTitle.text = String.format("系列名称：%s", listNovelOfSeries.novel_series_detail?.title)
+            baseBind.seriesDescription.setHtml(listNovelOfSeries.novel_series_detail?.display_text.orEmpty())
+            val minute = (listNovelOfSeries.novel_series_detail?.total_character_count ?: 0) / 500.0
+            baseBind.seriesDetail.text = String.format(
+                getString(R.string.how_many_novels),
+                listNovelOfSeries.novel_series_detail?.content_count,
+                listNovelOfSeries.novel_series_detail?.total_character_count,
+                Math.floor(minute / 60).toInt(),
+                minute.toInt() % 60,
+            )
+            if (listNovelOfSeries.list.isNotEmpty()) {
+                val bean = listNovelOfSeries.list[0]
+                val userBean = bean.user
+                initUser(userBean)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun initUser(userBean: UserBean) {
+        val seeUser = View.OnClickListener {
+            Common.showUser(mContext, userBean)
+        }
+        baseBind.userHead.setOnClickListener(seeUser)
+        baseBind.userName.setOnClickListener(seeUser)
+        baseBind.userName.text = userBean.name
+        Glide.with(mContext).load(GlideUtil.getHead(userBean)).into(baseBind.userHead)
+        if (userBean.isIs_followed()) {
+            baseBind.postLikeUser.text = "取消关注"
+        } else {
+            baseBind.postLikeUser.text = "添加关注"
+        }
+
+        baseBind.postLikeUser.setOnClickListener {
+            if (userBean.isIs_followed()) {
+                baseBind.postLikeUser.text = "添加关注"
+                userBean.setIs_followed(false)
+                PixivOperate.postUnFollowUser(userBean.id)
+            } else {
+                baseBind.postLikeUser.text = "取消关注"
+                userBean.setIs_followed(true)
+                PixivOperate.postFollowUser(userBean.id, Params.TYPE_PUBLIC)
+            }
+        }
+        baseBind.postLikeUser.setOnLongClickListener {
+            if (!userBean.isIs_followed()) {
+                baseBind.postLikeUser.text = "取消关注"
+                userBean.setIs_followed(true)
+                PixivOperate.postFollowUser(userBean.id, Params.TYPE_PRIVATE)
+            }
+            true
+        }
+    }
+
+    private fun saveNovelToDownload(novelBean: NovelBean, novelDetail: NovelDetail) {
+        IllustDownload.downloadNovel(
+            mContext as BaseActivity<*>,
+            novelBean,
+            novelDetail,
+            Callback<Uri> {
+                Common.showToast(getString(R.string.string_279), 2)
+            },
+        )
+    }
+
+    private fun saveNovelSeriesToDownload(novelSeriesItem: NovelSeriesItem, content: String) {
+        IllustDownload.downloadNovel(
+            mContext as BaseActivity<*>,
+            novelSeriesItem,
+            content,
+            Callback<Uri> {
+                Common.showToast(getString(R.string.string_279), 2)
+            },
+        )
+    }
+}
