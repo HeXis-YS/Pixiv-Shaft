30ba6c91fc6486dfcc938899b1f4207383168be7
HeXis-YS <40174982+HeXis-YS@users.noreply.github.com>
2026-04-08 20:48:38 +0800

    refactor: migrate params constants to kotlin

---
+++ app/src/main/java/ceui/lisa/utils/Params.kt
@@ -0,0 +1,205 @@
+package ceui.lisa.utils
+
+object Params {
+
+    const val ID = "simple id"
+    const val USER_ID = "user id"
+    const val ILLUST_ID = "illust id"
+    const val NOVEL_ID = "novel id"
+    const val ILLUST_TITLE = "illust title"
+    const val DATA_TYPE = "data type"
+    const val INDEX = "index"
+    const val CONTENT = "content"
+    const val DAY = "day"
+    const val URL = "url"
+    const val TITLE = "title"
+    const val KEY_WORD = "key word"
+    const val SORT_TYPE = "sort type"
+    const val CONTENT_TYPE = "content type"
+    const val SIZE = "size"
+    const val SEARCH_TYPE = "search type"
+    const val STAR_SIZE = "star size"
+    const val MANGA = "is manga"
+    const val SHOW_DIALOG = "show dialog"
+    const val USER_MODEL = "user model"
+    const val FILE_PATH = "file path"
+    const val STAR_TYPE = "star type"
+    const val FLAG = "flag"
+    const val RESPONSE = "response"
+    const val MIME = "mime"
+    const val ENCODING = "encoding"
+    const val HISTORY_URL = "history url"
+    const val TYPE_ILLUST = "illust"
+    const val TYPE_NOVEL = "novel"
+    const val TYPE_MANGA = "manga"
+    const val NOVEL_KEY = "pixiv_shaft_novel_"
+    const val USER_KEY = "pixiv_shaft_local_user"
+    const val PAGE_UUID = "page_uuid"
+    const val POSITION = "position"
+    const val IS_LIKED = "is liked"
+    const val IS_POPULAR = "is popular"
+    const val LAST_CLASS = "last class"
+    const val NOVEL_DETAIL = "novel detail"
+    const val PREFER_PRESERVE = "prefer preserve"
+    const val TAG_NAMES = "tag names"
+    const val MAIN_ACTIVITY_NAVIGATION_POSITION = "main activity navigation position"
+    const val FRAGMENT_SEARCH_CLIPBOARD_VALUE = "fragment search clipboard value"
+    const val MANGA_SERIES_ID = "manga series id"
+
+    const val VIEW_PAGER_MUTED = "muted history viewpager"
+    const val VIEW_PAGER_R18 = "r18 viewpager"
+
+    const val FILTER_ILLUST = "ceui.lisa.fragments.NetListFragment FILTER_ILLUST"
+    const val FILTER_NOVEL = "ceui.lisa.fragments.NetListFragment FILTER_NOVEL"
+    const val LIKED_ILLUST = "ceui.lisa.fragments.NetListFragment LIKED_ILLUST"
+    const val PLAY_GIF = "ceui.lisa.fragments.FragmentSingleUgora PLAY_GIF"
+    const val LIKED_USER = "ceui.lisa.fragments.NetListFragment LIKED_USER"
+    const val LIKED_NOVEL = "ceui.lisa.fragments.NetListFragment LIKED_NOVEL"
+    const val DOWNLOAD_FINISH = "ceui.lisa.fragments.NetListFragment DOWNLOAD_FINISH"
+    const val DOWNLOAD_ING = "ceui.lisa.fragments.NetListFragment DOWNLOAD_ING"
+    const val REFRESH_MAIN_ACTIVITY = "ceui.lisa.REFRESH_MAIN_ACTIVITY"
+    const val FRAGMENT_ADD_DATA = "ceui.lisa.FRAGMENT_ADD_DATA"
+    const val FRAGMENT_SCROLL_TO_POSITION = "ceui.lisa.FRAGMENT_SCROLL_TO_POSITION"
+    const val FRAGMENT_ADD_RELATED_DATA = "ceui.lisa.FRAGMENT_ADD_RELATED_DATA"
+
+    const val MAP_KEY = "Referer"
+    const val MAP_KEY_SMALL = "referer"
+    const val IMAGE_REFERER = "https://app-api.pixiv.net/"
+    const val USER_AGENT = "User-Agent"
+    const val PHONE_MODEL = "PixivIOSApp/5.8.0"
+    const val HOST = "Host"
+    const val CLOUD_DNS = "cloud dns"
+    const val HOST_NAME = "i.pximg.net"
+    const val DOWNLOAD_FAILED = -86
+    const val DOWNLOAD_SUCCESS = -87
+
+    const val MUTE_TAG = 0
+    const val MUTE_ILLUST = 1
+    const val MUTE_NOVEL = 2
+    const val MUTE_USER = 3
+
+    const val BLOCK_USER = 4
+
+    const val DIALOG_NOVEL_BG_COLOR = 0
+    const val DIALOG_NOVEL_TEXT_COLOR = 1
+
+    const val HINT_MULTI_DOWNLOAD = "HINT_MULTI_DOWNLOAD"
+    const val HINT_MULTI_DOWNLOAD_LONG_PRESS = "HINT_MULTI_DOWNLOAD_LONG_PRESS"
+    const val HINT_DOWNLOAD_LONG_PRESS = "HINT_DOWNLOAD"
+
+    const val IMAGE_UNKNOWN = "https://s.pximg.net/common/images/limit_unknown_360.png"
+    const val HEAD_UNKNOWN = "https://s.pximg.net/common/images/no_profile.png"
+    const val URL_R18_SETTING = "https://www.pixiv.net/settings/viewing"
+    const val URL_PREMIUM_SETTING = "https://www.pixiv.net/premium.php#premium-payment"
+
+    const val TYPE_PUBLIC = "public"
+    const val TYPE_PRIVATE = "private"
+    const val TYPE_ALL = "all"
+
+    const val IMAGE_RESOLUTION_ORIGINAL = "original"
+    const val IMAGE_RESOLUTION_LARGE = "large"
+    const val IMAGE_RESOLUTION_MEDIUM = "medium"
+    const val IMAGE_RESOLUTION_SQUARE_MEDIUM = "square_medium"
+
+    const val SHOW_LONG_DIALOG = "show long dialog"
+    const val IS_MIGRATE = "IS_MIGRATE"
+
+    const val MMKV_KEY_ISSHOWTIPS_SEARCHSORT = "tips searchsort"
+
+    const val REQUEST_CODE_CHOOSE = 10086
+    const val EXAMPLE_ILLUST = "{\n" +
+        "    \"caption\":\"\",\n" +
+        "    \"create_date\":\"2020-07-07T00:30:02+09:00\",\n" +
+        "    \"gifDelay\":0,\n" +
+        "    \"height\":2000,\n" +
+        "    \"id\":82805170,\n" +
+        "    \"image_urls\":{\n" +
+        "        \"large\":\"https://i.pximg.net/c/600x1200_90/img-master/img/2020/07/07/00/30/02/82805170_p0_master1200.jpg\",\n" +
+        "        \"medium\":\"https://i.pximg.net/c/540x540_70/img-master/img/2020/07/07/00/30/02/82805170_p0_master1200.jpg\",\n" +
+        "        \"square_medium\":\"https://i.pximg.net/c/360x360_70/img-master/img/2020/07/07/00/30/02/82805170_p0_square1200.jpg\"\n" +
+        "    },\n" +
+        "    \"isChecked\":false,\n" +
+        "    \"isShield\":false,\n" +
+        "    \"is_bookmarked\":false,\n" +
+        "    \"is_muted\":false,\n" +
+        "    \"meta_pages\":[\n" +
+        "        {\n" +
+        "            \"image_urls\":{\n" +
+        "                \"large\":\"https://i.pximg.net/c/600x1200_90/img-master/img/2020/07/07/00/30/02/82805170_p0_master1200.jpg\",\n" +
+        "                \"medium\":\"https://i.pximg.net/c/540x540_70/img-master/img/2020/07/07/00/30/02/82805170_p0_master1200.jpg\",\n" +
+        "                \"original\":\"https://i.pximg.net/img-original/img/2020/07/07/00/30/02/82805170_p0.png\",\n" +
+        "                \"square_medium\":\"https://i.pximg.net/c/360x360_70/img-master/img/2020/07/07/00/30/02/82805170_p0_square1200.jpg\"\n" +
+        "            }\n" +
+        "        },\n" +
+        "        {\n" +
+        "            \"image_urls\":{\n" +
+        "                \"large\":\"https://i.pximg.net/c/600x1200_90/img-master/img/2020/07/07/00/30/02/82805170_p1_master1200.jpg\",\n" +
+        "                \"medium\":\"https://i.pximg.net/c/540x540_70/img-master/img/2020/07/07/00/30/02/82805170_p1_master1200.jpg\",\n" +
+        "                \"original\":\"https://i.pximg.net/img-original/img/2020/07/07/00/30/02/82805170_p1.png\",\n" +
+        "                \"square_medium\":\"https://i.pximg.net/c/360x360_70/img-master/img/2020/07/07/00/30/02/82805170_p1_square1200.jpg\"\n" +
+        "            }\n" +
+        "        }\n" +
+        "    ],\n" +
+        "    \"meta_single_page\":{\n" +
+        "\n" +
+        "    },\n" +
+        "    \"page_count\":2,\n" +
+        "    \"restrict\":0,\n" +
+        "    \"sanity_level\":2,\n" +
+        "    \"tags\":[\n" +
+        "        {\n" +
+        "            \"added_by_uploaded_user\":false,\n" +
+        "            \"count\":0,\n" +
+        "            \"isSelected\":false,\n" +
+        "            \"name\":\"オリジナル\",\n" +
+        "            \"translated_name\":\"原创\"\n" +
+        "        },\n" +
+        "        {\n" +
+        "            \"added_by_uploaded_user\":false,\n" +
+        "            \"count\":0,\n" +
+        "            \"isSelected\":false,\n" +
+        "            \"name\":\"女の子\",\n" +
+        "            \"translated_name\":\"女孩子\"\n" +
+        "        },\n" +
+        "        {\n" +
+        "            \"added_by_uploaded_user\":false,\n" +
+        "            \"count\":0,\n" +
+        "            \"isSelected\":false,\n" +
+        "            \"name\":\"銀髪碧眼\",\n" +
+        "            \"translated_name\":\"银发碧眼\"\n" +
+        "        },\n" +
+        "        {\n" +
+        "            \"added_by_uploaded_user\":false,\n" +
+        "            \"count\":0,\n" +
+        "            \"isSelected\":false,\n" +
+        "            \"name\":\"オリジナル10000users入り\",\n" +
+        "            \"translated_name\":\"原创10000users加入书籤\"\n" +
+        "        }\n" +
+        "    ],\n" +
+        "    \"title\":\"作品标题\",\n" +
+        "    \"tools\":[\n" +
+        "\n" +
+        "    ],\n" +
+        "    \"total_bookmarks\":14306,\n" +
+        "    \"total_view\":47802,\n" +
+        "    \"type\":\"illust\",\n" +
+        "    \"user\":{\n" +
+        "        \"account\":\"sinsihukunokonaka\",\n" +
+        "        \"id\":1122006,\n" +
+        "        \"is_followed\":false,\n" +
+        "        \"is_login\":false,\n" +
+        "        \"is_mail_authorized\":false,\n" +
+        "        \"is_premium\":false,\n" +
+        "        \"lastTokenTime\":-1,\n" +
+        "        \"name\":\"画师昵称\",\n" +
+        "        \"profile_image_urls\":{\n" +
+        "            \"medium\":\"https://i.pximg.net/user-profile/img/2016/02/26/03/01/16/10587767_da08b7a0bc20d8eadbe8e7f9539b5400_170.png\"\n" +
+        "        },\n" +
+        "        \"require_policy_agreement\":false,\n" +
+        "        \"x_restrict\":0\n" +
+        "    },\n" +
+        "    \"visible\":true,\n" +
+        "    \"width\":1200,\n" +
+        "    \"x_restrict\":0\n" +
+        "}"
+}
