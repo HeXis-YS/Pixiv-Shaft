4bd935da861a1e5423b8ad874fd0a18e8375bebb
HeXis-YS <40174982+HeXis-YS@users.noreply.github.com>
2026-04-08 21:55:26 +0800

    refactor: migrate local history adapters to kotlin

---
+++ app/src/main/java/ceui/lisa/adapters/DownloadedAdapter.kt
@@ -0,0 +1,142 @@
+package ceui.lisa.adapters
+
+import android.content.Context
+import android.text.TextUtils
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.databinding.DataBindingUtil
+import com.bumptech.glide.Glide
+import ceui.lisa.R
+import ceui.lisa.activities.Shaft
+import ceui.lisa.activities.TemplateActivity
+import ceui.lisa.database.DownloadEntity
+import ceui.lisa.databinding.RecyDownloadedBinding
+import ceui.lisa.models.IllustsBean
+import ceui.lisa.models.NovelBean
+import ceui.lisa.utils.DensityUtil
+import ceui.lisa.utils.GlideUtil
+import ceui.lisa.utils.Params
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class DownloadedAdapter(
+    targetList: List<DownloadEntity>,
+    context: Context,
+) : BaseAdapter<DownloadEntity, RecyDownloadedBinding>(targetList, context) {
+
+    private val imageSize: Int = DensityUtil.dp2px(140.0f)
+    private val novelImageSize: Int = DensityUtil.dp2px(110.0f)
+    private val mTime =
+        SimpleDateFormat(
+            mContext.resources.getString(R.string.string_350),
+            Locale.getDefault(),
+        )
+
+    override fun initLayout() {
+        mLayoutID = R.layout.recy_downloaded
+    }
+
+    override fun bindData(
+        target: DownloadEntity,
+        bindView: ViewHolder<RecyDownloadedBinding>,
+        position: Int,
+    ) {
+        val fileName = allItems[position].fileName
+        if (!TextUtils.isEmpty(fileName) && fileName.contains(Params.NOVEL_KEY)) {
+            val params: ViewGroup.LayoutParams = bindView.baseBind.illustImage.layoutParams
+            params.width = novelImageSize
+            bindView.baseBind.illustImage.layoutParams = params
+
+            val current = Shaft.sGson.fromJson(allItems[position].illustGson, NovelBean::class.java)
+            bindView.baseBind.illustImage.scaleType = ImageView.ScaleType.CENTER_CROP
+            Glide.with(mContext)
+                .load(GlideUtil.getUrl(current.image_urls.medium))
+                .placeholder(R.color.light_bg)
+                .into(bindView.baseBind.illustImage)
+            bindView.baseBind.title.text = current.title
+            bindView.baseBind.author.text = String.format("by: %s", current.user.name)
+            bindView.baseBind.time.text = mTime.format(allItems[position].downloadTime)
+
+            bindView.baseBind.pSize.setText(R.string.string_171)
+
+            if (mOnItemClickListener != null) {
+                bindView.itemView.setOnClickListener { _: View ->
+                    TemplateActivity.startNovelDetail(mContext, current)
+                }
+                bindView.baseBind.deleteItem.setOnClickListener { v ->
+                    mOnItemClickListener.onItemClick(v, position, 2)
+                }
+            }
+        } else {
+            val params: ViewGroup.LayoutParams = bindView.baseBind.illustImage.layoutParams
+            params.height = imageSize
+            params.width = imageSize
+            bindView.baseBind.illustImage.layoutParams = params
+
+            val currentIllust =
+                Shaft.sGson.fromJson(allItems[position].illustGson, IllustsBean::class.java)
+            if (!TextUtils.isEmpty(allItems[position].fileName) &&
+                allItems[position].fileName.contains(".zip")
+            ) {
+                bindView.baseBind.illustImage.scaleType = ImageView.ScaleType.CENTER_INSIDE
+                Glide.with(mContext)
+                    .load(R.mipmap.zip)
+                    .placeholder(R.color.light_bg)
+                    .into(bindView.baseBind.illustImage)
+            } else {
+                if (currentIllust.isGif) {
+                    bindView.baseBind.illustImage.scaleType = ImageView.ScaleType.CENTER_CROP
+                    Glide.with(mContext)
+                        .load(currentIllust.image_urls.medium)
+                        .placeholder(R.color.light_bg)
+                        .into(bindView.baseBind.illustImage)
+                } else {
+                    bindView.baseBind.illustImage.scaleType = ImageView.ScaleType.CENTER_CROP
+                    Glide.with(mContext)
+                        .load(allItems[position].filePath)
+                        .placeholder(R.color.light_bg)
+                        .into(bindView.baseBind.illustImage)
+                }
+            }
+            bindView.baseBind.title.text = allItems[position].fileName
+            bindView.baseBind.author.text = String.format("by: %s", currentIllust.user.name)
+            bindView.baseBind.time.text = mTime.format(allItems[position].downloadTime)
+
+            if (currentIllust.page_count == 1) {
+                bindView.baseBind.pSize.visibility = View.GONE
+            } else {
+                bindView.baseBind.pSize.visibility = View.VISIBLE
+                bindView.baseBind.pSize.text =
+                    String.format(Locale.getDefault(), "%dP", currentIllust.page_count)
+            }
+
+            if (mOnItemClickListener != null) {
+                bindView.itemView.setOnClickListener { v ->
+                    mOnItemClickListener.onItemClick(v, position, 0)
+                }
+                bindView.baseBind.author.setOnClickListener { _ ->
+                    bindView.baseBind.author.tag = currentIllust.user.id
+                    mOnItemClickListener.onItemClick(bindView.baseBind.author, position, 1)
+                }
+                bindView.baseBind.deleteItem.setOnClickListener { v ->
+                    mOnItemClickListener.onItemClick(v, position, 2)
+                }
+            }
+        }
+        (bindView as DownloadedHolder).spring.currentValue = -400.0
+        bindView.spring.endValue = 0.0
+    }
+
+    override fun getNormalItem(parent: ViewGroup): ViewHolder<RecyDownloadedBinding> {
+        return DownloadedHolder(
+            DataBindingUtil.inflate(
+                LayoutInflater.from(mContext),
+                mLayoutID,
+                parent,
+                false,
+            ),
+        )
+    }
+}
---
+++ app/src/main/java/ceui/lisa/adapters/HistoryAdapter.kt
@@ -0,0 +1,134 @@
+package ceui.lisa.adapters
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.text.TextUtils
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.bumptech.glide.Glide
+import ceui.lisa.R
+import ceui.lisa.activities.Shaft
+import ceui.lisa.activities.TemplateActivity
+import ceui.lisa.database.IllustHistoryEntity
+import ceui.lisa.databinding.RecyViewHistoryBinding
+import ceui.lisa.models.IllustsBean
+import ceui.lisa.models.NovelBean
+import ceui.lisa.utils.DensityUtil
+import ceui.lisa.utils.GlideUtil
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class HistoryAdapter(
+    targetList: List<IllustHistoryEntity>,
+    context: Context,
+) : BaseAdapter<IllustHistoryEntity, RecyViewHistoryBinding>(targetList, context) {
+
+    private var illustImageSize = 0
+    private var novelImageSize = 0
+    private val mTime =
+        SimpleDateFormat(
+            mContext.resources.getString(R.string.string_350),
+            Locale.getDefault(),
+        )
+
+    init {
+        illustImageSize =
+            (mContext.resources.displayMetrics.widthPixels -
+                mContext.resources.getDimensionPixelSize(R.dimen.four_dp)) / 2
+        novelImageSize = DensityUtil.dp2px(110.0f)
+    }
+
+    override fun initLayout() {
+        mLayoutID = R.layout.recy_view_history
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun bindData(
+        target: IllustHistoryEntity,
+        bindView: ViewHolder<RecyViewHistoryBinding>,
+        position: Int,
+    ) {
+        if (target.type == 0) {
+            val params: ViewGroup.LayoutParams = bindView.baseBind.illustImage.layoutParams
+            params.height = illustImageSize
+            params.width = illustImageSize
+            bindView.baseBind.illustImage.layoutParams = params
+
+            val current = Shaft.sGson.fromJson(allItems[position].illustJson, IllustsBean::class.java)
+            Glide.with(mContext)
+                .load(GlideUtil.getMediumImg(current))
+                .placeholder(R.color.light_bg)
+                .into(bindView.baseBind.illustImage)
+            bindView.baseBind.title.text = current.title
+            bindView.baseBind.author.text = String.format("by: %s", current.user.name)
+
+            if (current.isGif) {
+                bindView.baseBind.pSize.visibility = View.VISIBLE
+                bindView.baseBind.pSize.text = "GIF"
+            } else {
+                if (current.page_count == 1) {
+                    bindView.baseBind.pSize.visibility = View.GONE
+                } else {
+                    bindView.baseBind.pSize.visibility = View.VISIBLE
+                    bindView.baseBind.pSize.text =
+                        String.format(Locale.getDefault(), "%dP", current.page_count)
+                }
+            }
+
+            if (mOnItemClickListener != null) {
+                bindView.itemView.setOnClickListener { v ->
+                    mOnItemClickListener.onItemClick(v, position, 0)
+                }
+                bindView.baseBind.author.setOnClickListener { _ ->
+                    bindView.baseBind.author.tag = current.user.id
+                    mOnItemClickListener.onItemClick(bindView.baseBind.author, position, 1)
+                }
+            }
+        } else if (target.type == 1) {
+            val params: ViewGroup.LayoutParams = bindView.baseBind.illustImage.layoutParams
+            params.width = novelImageSize
+            bindView.baseBind.illustImage.layoutParams = params
+
+            val current = Shaft.sGson.fromJson(allItems[position].illustJson, NovelBean::class.java)
+            Glide.with(mContext)
+                .load(GlideUtil.getUrl(current.image_urls.medium))
+                .placeholder(R.color.light_bg)
+                .into(bindView.baseBind.illustImage)
+            bindView.baseBind.title.text = current.title
+            bindView.baseBind.author.text = String.format("by: %s", current.user.name)
+
+            bindView.baseBind.pSize.visibility = View.VISIBLE
+            bindView.baseBind.pSize.text = "小说"
+
+            if (mOnItemClickListener != null) {
+                bindView.itemView.setOnClickListener { _: View ->
+                    TemplateActivity.startNovelDetail(mContext, current)
+                }
+                bindView.baseBind.author.setOnClickListener { _ ->
+                    bindView.baseBind.author.tag = current.user.id
+                    mOnItemClickListener.onItemClick(bindView.baseBind.author, position, 1)
+                }
+            }
+        }
+
+        bindView.baseBind.time.text = mTime.format(allItems[position].time)
+        bindView.baseBind.deleteItem.setOnClickListener { v ->
+            mOnItemClickListener.onItemClick(v, position, 2)
+        }
+        (bindView as SpringHolder).spring.currentValue = -400.0
+        bindView.spring.endValue = 0.0
+    }
+
+    override fun getNormalItem(parent: ViewGroup): ViewHolder<RecyViewHistoryBinding> {
+        return SpringHolder(
+            DataBindingUtil.inflate(
+                LayoutInflater.from(mContext),
+                mLayoutID,
+                parent,
+                false,
+            ),
+        )
+    }
+}
